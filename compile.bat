9a3e5c1193ad51da79f73c0f674f27fc090e590f
nekocat-Felis <haradak0320@gmail.com>
2024-02-07 16:57:53 +0900

    commit

--- compile.bat
+++ compile.bat
@@ -2,5 +2,5 @@ for /r %%i in (*.c *.cpp) do (
     g++ -c %%i -o source/object/%%~ni.o -I source/header -municode
 )
 
-g++ source/object/*.o -o release/start.exe -municode -mwindows
-call release\start.exe
+g++ source/object/*.o -o release/main.exe -municode -mwindows
+start release\main.exe
